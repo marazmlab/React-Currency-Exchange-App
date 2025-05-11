f1aafd24978c1f6d57a7b21cba65a2c8d7f290bc
Marek Bełz <marazm.lab@gmail.com>
2025-05-11 09:10:52 +0000

    config proceeded

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,11 @@
 import React from 'react';
 
 const App = () => {
-    return <h1>Currency Wallet</h1>;
+    return (
+        <div>
+            <h1>Currency Wallet</h1>
+        </div>
+    )
 }
 
 export default App;
